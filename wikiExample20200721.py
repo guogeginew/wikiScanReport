d0424f8a19e90235991e61cc700e17d7fc025f49
guogeginew <jinggong123@163.com>
2020-07-21 10:57:33 +0800

    提交

--- wikiExample20200721.py
+++ wikiExample20200721.py
@@ -8,7 +8,7 @@ from xlutils import copy
 import os
  
 # 功能点wiki
-url_api = 'http://117.174.121.179:8090/pages/viewpage.action?pageId=****'
+url_api = '******'
  
  
 #文件存放路径
@@ -34,7 +34,9 @@ def file_excel_exists(file_name):
         #创建excel文件
         work_book = xlwt.Workbook(encoding='ascii')
         # 对一个单元格重复操作会引发错误，以cell_overwrite_ok方式新增则不会出现错误
-        work_book.add_sheet('api',cell_overwrite_ok=True)
+        work_book.add_sheet(u'移交申请（进行中）',cell_overwrite_ok=True)
+        work_book.add_sheet(u'移交任务',cell_overwrite_ok=True)
+        work_book.add_sheet(u'移交任务（已完成）',cell_overwrite_ok=True)
         work_book.save(file_excel_name)
         return file_excel_name
     else:
@@ -44,7 +46,7 @@ def file_excel_exists(file_name):
 #爬取页面信息
 def get_html_content():
     conn = requests.session()
-    conn.auth = ("xxxx","123456")
+    conn.auth = ("xxxxx","123456")
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
     }
@@ -58,49 +60,62 @@ def get_html_content():
     #2.由于查找到的符合步骤1的标签共10个，而使用时只需要用到第2个，所以取数组下标为1的标签内容
     #3.再次初始化为BeautifulSoup对象，方便后面的使用
     print ("table: ", len(res_soup_api.find_all('table',class_='aui metadata-summary-macro null')))
-    table_api_value = BeautifulSoup(str(res_soup_api.find_all('table',class_='aui metadata-summary-macro null')[0]),'lxml')
-    #获取table下的第1个tbody标签下的标签内容，contents只能获取到第一个
-    # 通过观察发现tbody标签下的内容正是爬取数据所需要的，得到的是一个数组
-    table_api_tbody_value = table_api_value.tbody.contents
-    print("table_api_tbody_value len: ", len(table_api_tbody_value))
- 
-    for i in range(len(table_api_tbody_value)):
+    export_content(res_soup_api,'aui metadata-summary-macro null',u"移交申请（进行中）")
+    export_content(res_soup_api,'aui aui-table-interactive tasks-report',u"移交任务")    
+    export_content(res_soup_api,'aui aui-table-interactive tasks-report',u"移交任务（已完成）")    
+
+#导出每一个sheet页
+def export_content(res_soup_api,class_name,sheet_name):
+    table_api_value = BeautifulSoup(str(res_soup_api.find_all('table',class_=class_name)[0]),'lxml')
+    if sheet_name == u"移交任务（已完成）":
+        table_api_value = BeautifulSoup(str(res_soup_api.find_all('table',class_=class_name)[1]),'lxml')
+    #生成标题
+    export_head = table_api_value.thead.contents
+    print("export_head len: ", len(export_head))
+    for i in range(len(export_head)):
         #将获取到的tbody标签解析为lxml，并按数组下标逐个获取各个tr标签
-        table_api_tbody_all_value = BeautifulSoup(str((table_api_value.tbody.contents)[i]),'lxml')
+        export_head_all_value = BeautifulSoup(str((table_api_value.thead.contents)[i]),'lxml')
         #通过contents属性将获取得的tr直接子节点，得到的是一个数组列表
-        table_api_tr_all_value = table_api_tbody_all_value.tr.contents
-        print ("tr.contents len: ", len(table_api_tr_all_value))
-        for n in range(len(table_api_tr_all_value)):
-            msg_value = table_api_tr_all_value[n].text
+        export_head_tr = export_head_all_value.tr.contents
+        print ("thead.contents len: ", len(export_head_tr))
+        for n in range(len(export_head_tr)):
+            msg_value = export_head_tr[n].text
             #调用方法向excel表内写入数据
             print("msg_value: ", msg_value)
-            excel_data_write("api",i,n,msg_value)
-    print('功能点爬取完成，数据写入完成！')
+            excel_head_write(sheet_name,i,n,msg_value)
+    print('文件头写入完成...')
  
-    #contents 列出table下第一个tr子节点,contents只能列出一个
-    # table_tr1_value = table_value.tr.contents
-    # 查找table下所有tr标签
-    # table_tr_all = table_value.find_all('tr')
-    # for i in range(0,1):
-    #     print(table_tr_all[i])
-    #     table_tr_all_value = BeautifulSoup(str(table_tr_all[i]),'lxml')
-    #     table_th_all_value = table_tr_all_value.th.contents
-    #     print(table_th_all_value)
-    #     print(table_th_all_value[i].string)
- 
-    #enumerate处理数组或字符串组合为一个索引序列，同时列出数据和数据下标，一般在for循环中使用
-    # for i,child in enumerate(tb1_value):
-    #     print(i,child)
-    #     print(child.text)
-    # for i in range(0, 1):
-    #     for j in range(0, len(table_tr1_value)):
-    #         for j in range(len(table_tr1_value)):
-    #             #取出获得的标签数组中的文本值
-    #             msg_value = table_tr1_value[j].text
-    #             #调用向excel中写入数据的方法写数
-    #             excel_data_write(0, i, j, msg_value)
-    # print('数据写入成功！')
- 
+    #生成内容
+    #获取table下的第1个tbody标签下的标签内容，contents只能获取到第一个
+    # 通过观察发现tbody标签下的内容正是爬取数据所需要的，得到的是一个数组
+    export_content_tbody = table_api_value.tbody.contents
+    print("export_content_tbody len: ", len(export_content_tbody))
+    for i in range(len(export_content_tbody)):
+        #将获取到的tbody标签解析为lxml，并按数组下标逐个获取各个tr标签
+        export_content_tbody_all_value = BeautifulSoup(str((table_api_value.tbody.contents)[i]),'lxml')
+        #通过contents属性将获取得的tr直接子节点，得到的是一个数组列表
+        export_content_tbody_tr = export_content_tbody_all_value.tr.contents
+        #print ("tr.contents len: ", len(export_content_tbody_tr))
+        for n in range(len(export_content_tbody_tr)):
+            msg_value = export_content_tbody_tr[n].text
+            #调用方法向excel表内写入数据
+            #print("msg_value: ", msg_value)
+            excel_data_write(sheet_name,i,n,msg_value)
+    print('文件内容导出完成...')
+    print("*****************************************************")
+    print("*****************************************************")
+     
+
+#向excel中写入head
+def excel_head_write(sheet_name,i, j, excel_data):
+    file_excel_name = file_excel_exists(excel_name)
+    workbook = xlrd.open_workbook(file_excel_name)
+    # 复制文件并保留格式
+    workbook = copy.copy(workbook)
+    # 索引到第一个Sheet页
+    worksheet = workbook.get_sheet(sheet_name)
+    worksheet.write(i, j, label=excel_data)
+    workbook.save(file_excel_name)
  
 # 向excel中写入数据
 def excel_data_write(sheet_name,i, j, excel_data):
@@ -110,7 +125,7 @@ def excel_data_write(sheet_name,i, j, excel_data):
     workbook = copy.copy(workbook)
     # 索引到第一个Sheet页
     worksheet = workbook.get_sheet(sheet_name)
-    worksheet.write(i, j, label=excel_data)
+    worksheet.write(i+1, j, label=excel_data)
     workbook.save(file_excel_name)
  
     #name获取标签名称
